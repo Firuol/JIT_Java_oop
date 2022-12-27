4874e487f1ef7643c654cccc17d7d3a0b21dc31e
Firuol <118420729+Firuol@users.noreply.github.com>
2022-12-27 04:12:52 -0800

    Add files via upload
    gets maximum number from array

---
+++ program to get max num.java
@@ -0,0 +1,15 @@
+ //check which number is maximum
+       public class if(x>y){
+            System.out.println("x is greater then y");
+        }
+        else if (x==y){
+            System.out.println("x is equal to y");
+        }
+        else {
+            System.out.println("y is greater then x");
+        }
+
+        // using conditional operator ?
+        (x>y) ? System.out.print("x is greater than y"):System.out.print("y is greater");
+        //or 
+        system.out.println("max is"+((x>y)?i:j))
